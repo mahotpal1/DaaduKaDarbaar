7717075f4d712f1bc9799cd08908ef34a58df0b5
mahotpal1 <mahotpalvishal87@gmail.com>
2023-02-14 00:34:54 +0530

    requirements

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS, cross_origin
-import request
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 
@@ -9,7 +8,7 @@ app = Flask(__name__)
 @app.route("/", methods=['GET'])
 @cross_origin()
 def DaaduKitchen():
-    return render_template("style_index.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8001, debug=True)
