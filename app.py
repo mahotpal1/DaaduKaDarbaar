1e6095597c33a267967537d0b3f5933d8360c64c
mahotpal1 <mahotpalvishal87@gmail.com>
2023-02-14 00:36:13 +0530

    hjgfiadJFN

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def DaaduKitchen():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8001, debug=True)
+    app.run(host='0.0.0.0', debug=False)
